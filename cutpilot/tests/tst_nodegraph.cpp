01040113b613e23c71d76c2dd247b55deaa787d7
Areg Noya <aaa7661516@gmail.com>
2026-07-11 21:13:55 -0700

    Cover the current node model with a smoke test
    - Assert add assigns increasing ids
    - Assert hit-test returns the top-most node and misses empty space
    - Assert single-select marks one node and clears the rest
    - Assert move repositions a node's world rect

--- cutpilot/tests/tst_nodegraph.cpp
+++ cutpilot/tests/tst_nodegraph.cpp
@@ -4,27 +4,74 @@
 
 using namespace cutpilot::core;
 
+namespace {
+
+Node makeNode(const QPointF &pos, const QSizeF &size = QSizeF(120, 90))
+{
+    Node n;
+    n.worldPos = pos;
+    n.worldSize = size;
+    return n;
+}
+
+} // namespace
+
 class TstNodeGraph : public QObject {
     Q_OBJECT
 
 private slots:
     void addAssignsIncreasingIds();
+    void hitTestReturnsTopMostAndMiss();
+    void selectOnlyMarksExactlyOne();
+    void moveRepositionsWorldRect();
 };
 
 void TstNodeGraph::addAssignsIncreasingIds()
 {
     NodeGraph graph;
-    Node a;
-    a.worldPos = QPointF(0, 0);
-    a.worldSize = QSizeF(100, 80);
-    Node b = a;
-    b.worldPos = QPointF(200, 0);
-
-    const int idA = graph.addNode(a);
-    const int idB = graph.addNode(b);
+    const int idA = graph.addNode(makeNode(QPointF(0, 0)));
+    const int idB = graph.addNode(makeNode(QPointF(400, 0)));
 
     QVERIFY(idB > idA);
     QCOMPARE(graph.nodes().size(), 2);
+}
+
+void TstNodeGraph::hitTestReturnsTopMostAndMiss()
+{
+    NodeGraph graph;
+    // Two overlapping cards; the later-added one is on top in insertion z-order.
+    const int lower = graph.addNode(makeNode(QPointF(0, 0), QSizeF(200, 200)));
+    const int upper = graph.addNode(makeNode(QPointF(50, 50), QSizeF(200, 200)));
+
+    QCOMPARE(graph.hitTest(QPointF(100, 100)), upper);
+    QCOMPARE(graph.hitTest(QPointF(10, 10)), lower);
+    QCOMPARE(graph.hitTest(QPointF(1000, 1000)), -1);
+}
+
+void TstNodeGraph::selectOnlyMarksExactlyOne()
+{
+    NodeGraph graph;
+    const int idA = graph.addNode(makeNode(QPointF(0, 0)));
+    const int idB = graph.addNode(makeNode(QPointF(400, 0)));
+
+    graph.selectOnly(idB);
+    QVERIFY(graph.anySelected());
+    QCOMPARE(graph.selectedId(), idB);
+    QVERIFY(!graph.nodeById(idA)->selected);
+    QVERIFY(graph.nodeById(idB)->selected);
+
+    graph.selectOnly(-1);
+    QVERIFY(!graph.anySelected());
+}
+
+void TstNodeGraph::moveRepositionsWorldRect()
+{
+    NodeGraph graph;
+    const int id = graph.addNode(makeNode(QPointF(100, 100)));
+
+    graph.moveNodeTo(id, QPointF(250, 175));
+    QCOMPARE(graph.nodeById(id)->worldPos, QPointF(250, 175));
+    QCOMPARE(graph.nodeById(id)->worldRect().topLeft(), QPointF(250, 175));
 }
 
 QTEST_APPLESS_MAIN(TstNodeGraph)
